c6a2553bee58274e8d6a495ed959828cddf7c6a3
RomanHalaburda <mtbrider94@gmail.com>
2017-02-21 03:30:58 +0300

    first commit

---
+++ MachineKinematics/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MachineKinematics
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvTitles.RowCount = dgvInput.RowCount = 13;
+            dgvTitles.ColumnCount = dgvInput.ColumnCount = 1;
+            dgvTitles.ColumnHeadersVisible =
+                dgvInput.ColumnHeadersVisible =
+                dgvTitles.RowHeadersVisible =
+                dgvInput.RowHeadersVisible = false;
+            dgvTitles.Width = dgvInput.Width = 80;
+            dgvTitles.Columns[0].Width = dgvInput.Columns[0].Width = 77;
+            dgvTitles.DefaultCellStyle.Font = new Font("Lucida Calligraphy", 12);
+            for (int i = 0; i < 13; ++i)
+                dgvTitles.Rows[i].Cells[0].Value = "Fpc" + (i + 1) + " =";
+            dgvTitles.Enabled = false;
+
+            dgvLegend.AutoSize = true;
+
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void начатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Boolean flag = true;
+            foreach (Control c in this.Controls)
+            {
+                if (c is TextBox)
+                {
+                    TextBox textBox = c as TextBox;
+                    if (textBox.Text == string.Empty)
+                    {
+                        break;
+                    }
+                }
+            }
+            
+            if(this.Controls.OfType<TextBox>().Any(t => string.IsNullOrEmpty(t.Text)))  
+            {
+                MessageBox.Show("Вы ");
+            }
+            else
+            {
+                MessageBox.Show("Вы ввели недостаточно данных.");
+            }
+        }
+
+        private static Boolean checkInput()
+        {
+            //Boolean flag = true
+            foreach (Control c in Form.ActiveForm.Controls)
+            {
+                if (c is TextBox)
+                {
+                    TextBox textBox = c as TextBox;
+                    if (textBox.Text == string.Empty)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+    }
+}
